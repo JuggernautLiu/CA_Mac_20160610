363853b3eaad0688cd465423ff081aed7ac51072
Juggernaut Liu <juggernaut.liu@gmail.com>
2016-06-11 10:28:04 +0800

    First

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CA_Mac_20160610
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello Mac Jug 2 !!!");
+        }
+    }
+}
